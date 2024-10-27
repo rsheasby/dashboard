d2c32138f0f1d7e87ae5d91b27a8dc0f3263aa23
Ryan David Sheasby <ryan@sheasby.dev>
2024-10-27 11:22:17 +0200

    Feeble attempt to reduce number of code analysis errors

--- web/src/stories/Widget.stories.svelte
+++ web/src/stories/Widget.stories.svelte
@@ -1,9 +1,7 @@
 <script module>
   import { defineMeta } from '@storybook/addon-svelte-csf';
   import Widget from '@components/Widget.svelte';
-  import { fn } from '@storybook/test';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
   const { Story } = defineMeta({
     title: 'Components/Widget',
     component: Widget,
