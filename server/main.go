f21c844533784bfec52f618397af1c77b35c25d8
Ryan David Sheasby <ryan@sheasby.dev>
2024-11-06 17:18:15 +0200

    basic calendar fetching

--- server/main.go
+++ server/main.go
@@ -1,1 +1,122 @@
-package server
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+var (
+	oauthConfig *oauth2.Config
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	oauthConfig = &oauth2.Config{
+		RedirectURL:  os.Getenv("REDIRECT_URL"),
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		Scopes:       []string{calendar.CalendarReadonlyScope},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/login", handleGoogleLogin)
+	http.HandleFunc("/callback", handleGoogleCallback)
+	http.HandleFunc("/next-meeting", handleNextMeeting)
+
+	fmt.Println("Started running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html><body><a href="/login">Google Log In</a></body></html>`
+	fmt.Fprint(w, htmlIndex)
+}
+
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := oauthConfig.AuthCodeURL("randomstate")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	token, err := oauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	saveToken(token)
+	http.Redirect(w, r, "/next-meeting", http.StatusTemporaryRedirect)
+}
+
+func saveToken(token *oauth2.Token) {
+	file, err := os.Create("token.json")
+	if err != nil {
+		log.Fatalf("Unable to create file: %v", err)
+	}
+	defer file.Close()
+	json.NewEncoder(file).Encode(token)
+}
+
+func tokenFromFile() (*oauth2.Token, error) {
+	file, err := os.Open("token.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	token := &oauth2.Token{}
+	err = json.NewDecoder(file).Decode(token)
+	return token, err
+}
+
+func handleNextMeeting(w http.ResponseWriter, r *http.Request) {
+	token, err := tokenFromFile()
+	if err != nil {
+		http.Error(w, "Unable to get token from file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	client := oauthConfig.Client(context.Background(), token)
+	srv, err := calendar.NewService(context.Background(), option.WithHTTPClient(client))
+	if err != nil {
+		http.Error(w, "Unable to create calendar service: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t := time.Now().Format(time.RFC3339)
+	events, err := srv.Events.List("primary").ShowDeleted(false).
+		SingleEvents(true).TimeMin(t).MaxResults(1).OrderBy("startTime").Do()
+	if err != nil {
+		http.Error(w, "Unable to retrieve next meeting: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(events.Items) == 0 {
+		fmt.Fprint(w, "No upcoming events found.")
+		return
+	}
+
+	event := events.Items[0]
+	start := event.Start.DateTime
+	if start == "" {
+		start = event.Start.Date
+	}
+	fmt.Fprintf(w, "Next meeting: %s (%s)\n", event.Summary, start)
+}
